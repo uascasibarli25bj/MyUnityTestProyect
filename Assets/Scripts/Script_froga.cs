f127a846eaac24ec3f4bb1f65ec2a3579f836fd6
uascasibarli25bj <uascasibarli25bj@ikzubirimanteo.com>
2025-10-29 12:38:13 +0100

    fs

--- Assets/Scripts/Script_froga.cs
+++ Assets/Scripts/Script_froga.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class Script_froga : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Start() 
     {
         // bla bla bla
     }
