afb195c7fb6a0d16f5c8939c0a55e8530ad9e72a
uascasibarli25bj <uascasibarli25bj@ikzubirimanteo.com>
2025-10-29 12:40:29 +0100

    adw

--- Assets/Scripts/Script_froga.cs
+++ Assets/Scripts/Script_froga.cs
@@ -5,7 +5,7 @@ public class Script_froga : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start() 
     {
-        // bla bla bla
+        // bla bla bla  
     }
 
     // Update is called once per frame
